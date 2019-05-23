de811adec4b6defef51924a7a8cd88e399eb24bc
Btate712 <btate712@gmail.com>
2019-05-22 20:49:14 -0500

    able to get user token back from google

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :current_user, :logged_in?
 
   def root
   end
 
-  helper_method :current_user, :log_in
+  helper_method :current_user, :log_in, :logged_in?
 
   def current_user
-    @current_user ||= session[:user_id]
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    end
   end
 
   def log_in(user)
     session[:user_id] = user.id
+  end
+
+  def logged_in?
+    @logged_in = !!current_user
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,15 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :username, presence: true, uniqueness: true
-  
+
   has_many :reviews
   has_many :wines, through: :reviews
+
+  def self.from_omniauth(auth)
+    # Creates a new user only if it doesn't exist
+    where(username: auth.info.email).first_or_initialize do |user|
+      user.username = auth.info.email.split("@").first
+      user.password = SecureRandom.hex
+    end
+  end
 end
